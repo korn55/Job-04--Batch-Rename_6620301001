eac03c2fea9e97dd9e049df11f61644f14574938
korn55 <6620301001@cdti.ac.th>
2024-01-29 10:17:36 +0700

    update_sort2

--- rename.py
+++ rename.py
@@ -9,7 +9,7 @@ def rename_files(directory_path, file_extension):
 files = [file for file in os.listdir(directory) if file.endswith(file_extension)]
 
 
-files_to_rename.sort()
+files.sort()
 
 for index, file_name in enumerate(files_to_rename, start=1):
     new_name = f"{index:03d}"
