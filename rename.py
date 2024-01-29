1f1e6254007633a70ab4d5f2be3fa481ebd4ec09
korn55 <6620301001@cdti.ac.th>
2024-01-29 10:06:25 +0700

    update_new_name

--- rename.py
+++ rename.py
@@ -10,7 +10,7 @@ files_to_rename = [file for file in os.listdir() if file.endswith(file_extension
 files_to_rename.sort()
 
 for index, file_name in enumerate(files_to_rename, start=1):
-new_name = f"{index:03d}{file_extension}"
+    new_name = f"{index:03d}{file_extension}"
 os.rename(file_name, new_name)     
     print(f"Renamed {file_name} to {new_name}")
 
