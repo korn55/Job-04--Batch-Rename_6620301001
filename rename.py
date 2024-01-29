e2baec3441280c7f59f059baa5fa81652c22fa7f
korn55 <6620301001@cdti.ac.th>
2024-01-29 09:56:58 +0700

    up sort
    update sort

--- rename.py
+++ rename.py
@@ -5,3 +5,6 @@ def rename_files(directory_path, file_extension):
 os.chdir(directory_path)
 
 files_to_rename = [file for file in os.listdir() if file.endswith(file_extension)]
+
+
+files_to_rename.sort()
