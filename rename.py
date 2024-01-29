6526ea50f6cba17ef8485514363bdd8c86f2d8a4
korn55 <6620301001@cdti.ac.th>
2024-01-29 10:19:41 +0700

    update for

--- rename.py
+++ rename.py
@@ -11,10 +11,16 @@ files = [file for file in os.listdir(directory) if file.endswith(file_extension)
 
 files.sort()
 
-for index, file_name in enumerate(files_to_rename, start=1):
-    new_name = f"{index:03d}"
-    os.rename(file_name, new_name)     
-    print(f"Renamed {file_name} to {new_name}")
+for i, file in enumerate(files, start=1):
+
+    new_name = f"{i:03d}{file_extension}"
+
+    old_path = os.path.join(directory, file)
+    ew_path = os.path.join(directory, new_name)
+
+    os.rename(old_path, new_path)
+    print(f"Rename: {file} = {new_name}")
+
 
 directory_path = "D:/Job-04--Batch-Rename_6620301001"  
 file_extension = ".jpg"
