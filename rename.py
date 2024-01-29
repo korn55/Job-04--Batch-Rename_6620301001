c708642b70f04cf1b961f30bbeb6a243ceee1d4f
korn55 <6620301001@cdti.ac.th>
2024-01-29 10:36:12 +0700

    update directory_path

--- rename.py
+++ rename.py
@@ -3,7 +3,7 @@ import os
 def rename_files(directory, file_extension):
     #Check if the Directory exists or not.
     if not os.path.exists(directory):
-        print(f"Directory '{directory}' ไม่พบ")
+        print(f"Directory '{directory}' Not found")
         return
     
 # Collect all files in the Directory with the file_extension extension.
@@ -19,13 +19,13 @@ def rename_files(directory, file_extension):
         
         # Create the full file path
         old_path = os.path.join(directory, file)
-        ew_path = os.path.join(directory, new_name)
+        new_path = os.path.join(directory, new_name)
 
         # rename file
         os.rename(old_path, new_path)
         print(f"Rename: {file} = {new_name}")
 
 # Call the function
-directory_path = "D:/Job-04--Batch-Rename_6620301001"  
+directory_path = "D:/git_hub_repossi/Job-04--Batch-Rename_6620301001"  
 file_extension_to_rename = ".jpg"
 rename_files(directory_path, file_extension_to_rename)
