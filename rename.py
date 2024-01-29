e54f2884921a1faf509f66d53696f8d2adf06201
korn55 <6620301001@cdti.ac.th>
2024-01-29 09:58:48 +0700

    up date new name

--- rename.py
+++ rename.py
@@ -10,3 +10,5 @@ files_to_rename = [file for file in os.listdir() if file.endswith(file_extension
 files_to_rename.sort()
 
 for index, file_name in enumerate(files_to_rename, start=1):
+new_name = f"{index:03d}{file_extension}"
+       
