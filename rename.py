7c82183ac2c61a5443f7c30dd4591d3e4cfd9824
korn55 <6620301001@cdti.ac.th>
2024-01-29 10:16:22 +0700

    update def rename

--- rename.py
+++ rename.py
@@ -2,7 +2,9 @@ import os
 
 def rename_files(directory_path, file_extension):
 
-os.chdir(directory_path)
+    if not os.path.exists(directory):
+        print(f"Directory '{directory}' ไม่พบ")
+        return
 
 files_to_rename = [file for file in os.listdir() if file.endswith(file_extension)]
 
@@ -10,10 +12,10 @@ files_to_rename = [file for file in os.listdir() if file.endswith(file_extension
 files_to_rename.sort()
 
 for index, file_name in enumerate(files_to_rename, start=1):
-    new_name = f"{index:03d}{file_extension}"
-os.rename(file_name, new_name)     
+    new_name = f"{index:03d}"
+    os.rename(file_name, new_name)     
     print(f"Renamed {file_name} to {new_name}")
 
-directory_path = "."  
+directory_path = "D:/Job-04--Batch-Rename_6620301001"  
 file_extension = ".jpg"
 rename_files(directory_path, file_extension)
